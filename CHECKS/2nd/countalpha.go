456dec30e8eafe174460c26b54a8d71187b5eae4
Sulaimon Akande <kingakande@gmail.com>
2025-11-25 16:02:18 +0100

    added counter

---
+++ CHECKS/2nd/countalpha.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+/*
+*
+
+countalpha
+Instructions
+Write a function CountAlpha() that takes a string as an argument and returns the number of alphabetic characters in the string.
+*/
+func main() {
+
+}
+
+func CountAlpha(s string) int {
+
+	for _, item := range s {
+
+		fmt.Println(item)
+	}
+}
