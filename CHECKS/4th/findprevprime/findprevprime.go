5a815819d13f8450c1ef8ffa065a88622b36e029
Sulaimon Akande <kingakande@gmail.com>
2025-11-28 12:45:11 +0100

    aded

---
+++ CHECKS/4th/findprevprime/findprevprime.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/01-edu/go-tests/lib/is"
+
+/**
+Write a function that returns the first prime number that is equal or inferior to the int passed as parameter.
+
+If there are no primes inferior to the int passed as parameter the function should return 0.
+
+
+**/
+
+func FindPrevPrime(nb int) int {
+
+	if nb < 2 {
+		return 0
+	}
+	if is.Prime(nb) {
+		return nb
+	}
+	return FindPrevPrime(nb - 1)
+}
