ea30c5227f9d1934d86f4749aeb909776880f729
Sulaimon Akande <kingakande@gmail.com>
2025-12-01 15:03:35 +0100

    s

---
+++ sudoku.go
@@ -0,0 +1,12 @@
+/** os is the package that enables go interact with the computers operating system....
+
+os.ARG is the list of everything you typed when running the programm.. access an  array that contains the progamme name an other instruction
+
+make() is function that creates a data structure
+
+
+rune is GO way of storing a single character.....we use it here in sudoku as each cell stores a single character.
+
+
+
+*/
